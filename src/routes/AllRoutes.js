6f2e0d018b1dcf5b1119a0dd266e29201dffcf25
OshnMdw <gchoshanmadawa@gmail.com>
2023-09-05 17:36:00 +0530

    routes updated

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -3,16 +3,19 @@ import { Route, Routes } from 'react-router-dom';
 import PrivateRoutes from './PrivateRoutes';
 import AdminPageView from '../views/AdminPageView';
 import EditVideo from '../views/EditVideo';
-import AllQuizView from '../views/AllQuizView'
-
+import AllQuizView from '../views/AllQuizView';
+import AllPopQuizView from '../views/AllPopQuizView';
+import AllUsersView from '../views/AllUsersView';
 
 function AllRoutes() {
   return (
     <Routes>
         <Route element={<PrivateRoutes/>} >
-          <Route path='/editvideo'  element={<EditVideo />}/>
           <Route path='/' exact element={<AdminPageView />}/>
+          <Route path='/editvideo'  element={<EditVideo />}/>
           <Route path='/allquizzes'  element={<AllQuizView />}/>
+          <Route path='/allpopquizzes'  element={<AllPopQuizView />}/>
+          <Route path='/allusers'  element={<AllUsersView />}/>
         </Route>
      </Routes>
   )
