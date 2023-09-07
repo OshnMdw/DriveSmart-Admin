166bbc6ec6b08c7cfadf9e5640d00883123dbc54
Luxshan2000 <Luxshanlux2000@gmail.com>
2023-09-07 18:12:06 +0530

    reviewed!

--- src/views/SignUp.js
+++ src/views/SignUp.js
@@ -1,7 +1,6 @@
 import React from "react";
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
-
 import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom";
 
