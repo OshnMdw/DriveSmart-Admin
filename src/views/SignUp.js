7fe1ad4e07e4970cc9c9a37eec3cf1fed6c0e1f6
Luxshan2000 <Luxshanlux2000@gmail.com>
2023-09-08 18:29:26 +0530

    reviewed-style

--- src/views/SignUp.js
+++ src/views/SignUp.js
@@ -3,6 +3,7 @@ import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom";
+import '../assets/CSS/signUp.css'
 
 export default function SignUp() {
     return (
