d56f57383fc242b7745ed3e59bbcd44aefda80f2
OshnMdw <gchoshanmadawa@gmail.com>
2023-10-22 15:39:45 +0530

    Nav bar small change

--- src/components/OffCanvasNavComponent.js
+++ src/components/OffCanvasNavComponent.js
@@ -9,7 +9,7 @@ function OffCanvasNavComponent({ collapsed }) {
   return (
     <div style={{width:isSmall ? `${collapsed? '100%': '0'}` : `${collapsed? 200: 0}px`, overflowX:'hidden', transition: `width ${isSmall? '100ms' :'200ms'} linear`}}  className="d-flex  flex-column flex-shrink-0 vh-100 m-0">
     <div className=''>
-      <Link to="/" className=" pt-2 text-center d-flex align-items-center  my-5 me-md-auto text-dark text-decoration-none">
+      <Link to="/" className=" pt-2 text-center d-flex align-items-center  my-4 me-md-auto text-dark text-decoration-none">
         {/* <i style={{fontSize:"10em"}} class="bi w-100  bi-person-circle"/> */}
         <img src={icon}  width="200px"/>
       </Link>
